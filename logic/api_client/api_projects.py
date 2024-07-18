108a00b440c8a7e62854e00a1b4669775e9b8b0d
Anastasiya <anastasiya.niadbailik@gmail.com>
2024-07-18 11:24:00 +0300

    fix: add get_token.py to the utilities.api_functions folder, remove get token from api_projects.py, edit api test

--- logic/api_client/api_projects.py
+++ logic/api_client/api_projects.py
@@ -21,9 +21,45 @@ class Projects:
             res = requests.post(self.base_url + 'projects', data=data, params=params)
             status = res.status_code
             project_id = res.json().get('id')
+            project_data = res.json()
+            code = res.json().get('code')
+            # color = not res.json().get('color')
+            assert isinstance(project_data, dict), 'The data obtained does not correspond to the expected structure'
+            assert all(key in project_data for key in AD.expected_project_data)
             return status, project_id
         except requests.exceptions.RequestException as e:
             raise Exception(f"An error occurred while processing this request: {e}")
+
+    def create_project_all_data(self) -> json:
+        """Positive: Create a new project with all data"""
+        try:
+            params = {'token': self.token}
+            data = AD.create_project_all_data
+            res = requests.post(self.base_url + 'projects', data=data, params=params)
+            status = res.status_code
+            project_id = res.json().get('id')
+            name = res.json().get('name')
+            assert name == data['name']
+            return status, project_id
+        except requests.exceptions.RequestException as e:
+            raise Exception(f"An error occurred while processing this request: {e}")
+
+    def get_project_by_id(self, project_id) -> json:
+        """Positive: Get a project by id """
+        try:
+            params = {'token': self.token,
+                      'project_ids[]': project_id
+                      }
+            res = requests.get(self.base_url + 'projects/in_range', params=params)
+            status = res.status_code
+            project_name = res.json()[0]['name']
+            client_id = res.json()[0]['client_id']
+            manager_id = res.json()[0]['manager_id']
+            project_status = res.json()[0]['status']
+            return status, project_name, client_id, manager_id, project_status
+        except requests.exceptions.RequestException as e:
+            raise Exception(f"An error occurred while processing this request: {e}")
+
 
     def deactivate_project(self, project_id) -> json:
         """Positive: Deactivate a project"""
@@ -59,13 +95,30 @@ class Projects:
             }
             res = requests.patch(self.base_url + 'projects/delete', params=params)
             status = res.status_code
+            assert status == 200
             return status
         except requests.exceptions.RequestException as e:
             raise Exception(f"An error occurred while processing this request: {e}")
 
+    def get_all_managers(self):
+        """Positive: Get all managers """
+        try:
+            res = requests.get(self.base_url + 'users' + '?token=' + self.token)
+            # res.raise_for_status()
+            status = res.status_code
+            managers = res.json()
+            # for i in managers:
+            #     managers = i.get('id')
+            #     return managers
+            # managers = text.get('id')
+            return status, managers
+        except requests.exceptions.RequestException as e:
+            raise Exception(f"An error occurred while processing this request: {e}")
 
-# Projects().create_project()
+
+Projects().create_project()
 # Projects().get_managers()
 # Projects().deactivate_project()
 # Projects().activate_project()
 # Projects().delete_project()
+# Projects().get_all_managers()
--- logic/api_client/data/body_data.py
+++ logic/api_client/data/body_data.py
@@ -1,4 +1,5 @@
 import os
+from array import array
 
 from dotenv import load_dotenv
 from utilities.settings import ProjectsPageData
@@ -18,7 +19,42 @@ class ApiData:
     create_project_required_data = {
                 'name': f'API{ProjectsPageData.PROJECT_NAME}',
                 'code': ProjectsPageData.SHORT_PROJECT_NAME,
-                'color': '#EA8FEA',
+                'color': '#70e6f9',
                 'active': True,
                 'kind': 'billable'
             }
+
+    create_project_all_data = {
+                "name": f'API{ProjectsPageData.PROJECT_NAME}',
+                "code": ProjectsPageData.SHORT_PROJECT_NAME,
+                "color": "#2642ab",
+                'active': True,
+                "kind": "billable",
+                'icon_id': 1,
+                'estimated': 80,
+                'notes': 'test note',
+                'client_id': 5563, # сделать создание клиента и возвращать  id
+                'manager_id': 9526, # сделать получение id менеджера
+                'status': 'Done',
+                'business_unit': 'test'
+            }
+
+    expected_project_data = {
+                "name": str,
+                "code": str,
+                'active': bool,
+                "color": str,
+                "kind": str,
+                'icon_id': int,
+                'estimated': int,
+                'notes': str,
+                'client_id': int,
+                'start_date': str,
+                'end_date': str,
+                'manager_id': int,
+                'status': str,
+                'business_unit': str,
+                'tasks': array[str],
+                'custom_fields': str,
+                'token': str
+    }
